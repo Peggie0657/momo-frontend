4608c9dc99993041e95f73345ca167e728a472b2
Peggie0657 <86345006+Peggie0657@users.noreply.github.com>
2021-12-31 15:12:18 +0800

    login2

--- public/js/login.js
+++ public/js/login.js
@@ -2,19 +2,16 @@ function show_hide() {
     var login = document.getElementById("container1");
     var signup = document.getElementById("container2");
     var footer = document.getElementById("footer");
-  
+
     if (login.style.display === "none") {
         login.style.display = "block";  //lonin出現
         document.getElementById("username").value="";
         document.getElementById("password").value="";
         signup.style.display = "none";  //signup消失
-        footer.style.margin = "200px 0px 0px 0px";
     } else {
         login.style.display = "none";   //login消失
         signup.style.display = "block"; //signup出現
         signup.style.visibility="visible";
-        footer.style.margin = "200px 0px 0px 0px";
-     
         document.getElementById("username_signup").value="";
         document.getElementById("email").value="";
         document.getElementById("password_signup").value="";
