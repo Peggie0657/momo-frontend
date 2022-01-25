e5d26930cf17bae4a0b86c048d4a2eb487a86906
Peggie0657 <86345006+Peggie0657@users.noreply.github.com>
2022-01-25 15:15:35 +0800

    update

--- src/component/MyOrder.js
+++ src/component/MyOrder.js
@@ -72,7 +72,7 @@ function Row(props) {
                     <Collapse in={open} timeout="auto" unmountOnExit>
                         <Box sx={{ margin: 1 }}>
                             <Typography variant="h6" gutterBottom component="div">
-                                訂單明細
+                                訂單明細  
                             </Typography>
                             <Table size="small" aria-label="purchases">
                                 <TableHead>
