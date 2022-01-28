2783b491ec111e8090b86ae84a5d84896fe6306a
dommei <yuchiehkao928@gmail.com>
2022-01-28 11:14:37 +0800

    update

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router";
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { alpha } from '@mui/material/styles';
+import styled from 'styled-components'
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -31,6 +32,8 @@ import { getCart, removeItem } from './cartHelpers';
 import { createTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
+import { color } from '@mui/system';
+import { grey, pink } from '@mui/material/colors';
 
 // import HUE from '@material-ui/core/colors/HUE';
 
@@ -115,12 +118,23 @@ function EnhancedTableHead(props) {
             <TableRow>
                 <TableCell padding="checkbox">
                     <Checkbox
-                        color="primary"
+                        // color="primary"
                         indeterminate={numSelected > 0 && numSelected < rowCount}
                         checked={rowCount > 0 && numSelected === rowCount}
                         onChange={onSelectAllClick}
                         inputProps={{
                             'aria-label': 'select all desserts',
+                        }}
+                        sx={{
+                            '&.Mui-checked': {
+                                color: pink[100],
+                            },
+                            '&.MuiCheckbox-indeterminate': {
+                                color: pink[100],
+                            },
+                            '&:hover': {
+                                backgroundColor: grey[100],
+                            }
                         }}
                     />
                 </TableCell>
@@ -179,6 +193,7 @@ const EnhancedTableToolbar = (props) => {
                     bgcolor: (theme) =>
                         alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
                 }),
+                // backgroundColor: grey[100]
             }}
         >
             {numSelected > 0 ? (
@@ -222,7 +237,7 @@ EnhancedTableToolbar.propTypes = {
     numSelected: PropTypes.number.isRequired,
 };
 
-const Cart = ({ setItemCount }) => {
+const Element = ({ setItemCount }) => {
     const [redirect, setRedirect] = useState(false)
     const [order, setOrder] = useState('asc');
     const [orderBy, setOrderBy] = useState('calories');
@@ -332,77 +347,93 @@ const Cart = ({ setItemCount }) => {
 
     return (
         <ThemeProvider theme={theme}>
-        <div>
-            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-xl">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel"><ShoppingCartIcon />購物車</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <Box sx={{ width: '100%' }}>
-                                <Paper sx={{ width: '100%', mb: 2 }}>
-                                    <EnhancedTableToolbar numSelected={selected.length} selectedArr={selectedArr} setTotal={setTotal} cartFetch={cartFetch} setSelected={setSelected} />
-                                    <TableContainer sx={{ maxHeight: 300 }}>
-                                        <Table
-                                            sx={{ minWidth: 750 }}
-                                            // aria-labelledby="tableTitle"
-                                            size="medium"
-                                            stickyHeader
-                                            aria-label="sticky table"
-                                        >
-                                            <EnhancedTableHead
-                                                numSelected={selected.length}
-                                                order={order}
-                                                orderBy={orderBy}
-                                                onSelectAllClick={handleSelectAllClick}
-                                                onRequestSort={handleRequestSort}
-                                                rowCount={products.length}
-                                            />
-                                            <TableBody>
-                                                {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+            <div>
+                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-xl">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel"><ShoppingCartIcon />購物車</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <Box sx={{ width: '100%' }}>
+                                    <Paper sx={{ width: '100%', mb: 2 }}>
+                                        <EnhancedTableToolbar numSelected={selected.length} selectedArr={selectedArr} setTotal={setTotal} cartFetch={cartFetch} setSelected={setSelected} />
+                                        <TableContainer sx={{ maxHeight: 300 }}>
+                                            <Table
+                                                sx={{ minWidth: 750 }}
+                                                // aria-labelledby="tableTitle"
+                                                size="medium"
+                                                stickyHeader
+                                                aria-label="sticky table"
+                                            >
+                                                <EnhancedTableHead
+                                                    numSelected={selected.length}
+                                                    order={order}
+                                                    orderBy={orderBy}
+                                                    onSelectAllClick={handleSelectAllClick}
+                                                    onRequestSort={handleRequestSort}
+                                                    rowCount={products.length}
+                                                />
+                                                <TableBody>
+                                                    {/* if you don't need to support IE11, you can replace the `stableSort` call with:
                  rows.slice().sort(getComparator(order, orderBy)) */}
-                                                {stableSort(products, getComparator(order, orderBy))
-                                                    .map((row, index) => {
-                                                        const isItemSelected = isSelected(row.name);
-                                                        const labelId = `enhanced-table-checkbox-${index}`;
+                                                    {stableSort(products, getComparator(order, orderBy))
+                                                        .map((row, index) => {
+                                                            const isItemSelected = isSelected(row.name);
+                                                            const labelId = `enhanced-table-checkbox-${index}`;
 
-                                                        return (
-                                                            <TableRow
-                                                                hover
-                                                                onClick={() => handleClick(row, row.name || "")}
-                                                                role="checkbox"
-                                                                aria-checked={isItemSelected}
-                                                                tabIndex={-1}
-                                                                key={row.name}
-                                                                selected={isItemSelected}
-                                                            >
-                                                                <TableCell padding="checkbox">
-                                                                    <Checkbox
-                                                                        color="primary"
-                                                                        checked={isItemSelected}
-                                                                        inputProps={{
-                                                                            'aria-labelledby': labelId,
-                                                                        }}
-                                                                    />
-                                                                </TableCell>
-                                                                <TableCell
-                                                                    component="th"
-                                                                    id={labelId}
-                                                                    scope="row"
-                                                                    padding="none"
+                                                            return (
+                                                                <TableRow
+                                                                    hover
+                                                                    onClick={() => handleClick(row, row.name || "")}
+                                                                    role="checkbox"
+                                                                    aria-checked={isItemSelected}
+                                                                    tabIndex={-1}
+                                                                    key={row.name}
+                                                                    selected={isItemSelected}
+                                                                    sx={{
+                                                                        '&.Mui-selected': {
+                                                                            backgroundColor: grey[100],
+                                                                        },
+                                                                        '&.Mui-selected:hover': {
+                                                                            backgroundColor: grey[200],
+                                                                        }
+                                                                    }}
                                                                 >
-                                                                    <img src={row.cover} width={90} />
-                                                                </TableCell>
-                                                                <TableCell align="right">{row.name || ""}</TableCell>
-                                                                <TableCell align="right">{row.price || ""}</TableCell>
-                                                                <TableCell align="right">{row.num}</TableCell>
-                                                                <TableCell align="right">{row.spec || ""}</TableCell>
-                                                            </TableRow>
-                                                        );
-                                                    })}
-                                                {/* {emptyRows > 0 && (
+                                                                    <TableCell padding="checkbox">
+                                                                        <Checkbox
+                                                                            // color="primary"
+                                                                            checked={isItemSelected}
+                                                                            inputProps={{
+                                                                                'aria-labelledby': labelId,
+                                                                            }}
+                                                                            sx={{
+                                                                                '&.Mui-checked': {
+                                                                                    color: pink[100],
+                                                                                },
+                                                                                '&:hover': {
+                                                                                    backgroundColor: grey[50],
+                                                                                }
+                                                                            }}
+                                                                        />
+                                                                    </TableCell>
+                                                                    <TableCell
+                                                                        component="th"
+                                                                        id={labelId}
+                                                                        scope="row"
+                                                                        padding="none"
+                                                                    >
+                                                                        <img src={row.cover} width={90} />
+                                                                    </TableCell>
+                                                                    <TableCell align="right">{row.name || ""}</TableCell>
+                                                                    <TableCell align="right">{row.price || ""}</TableCell>
+                                                                    <TableCell align="right">{row.num}</TableCell>
+                                                                    <TableCell align="right">{row.spec || ""}</TableCell>
+                                                                </TableRow>
+                                                            );
+                                                        })}
+                                                    {/* {emptyRows > 0 && (
                                                     <TableRow
                                                         style={{
                                                             height: (53) * emptyRows,
@@ -412,16 +443,16 @@ const Cart = ({ setItemCount }) => {
                                                     </TableRow>
                                                 )} */}
 
-                                            </TableBody>
-                                        </Table>
-                                    </TableContainer>
-                                    <br />
-                                    <Typography variant="h6" gutterBottom component="div" marginLeft={100}>
-                                        總金額 : {total} 台幣
-                                    </Typography>
-                                </Paper>
-                            </Box>
-                            {/* <table className="table table-hover table-responsive" id="' + idCartTable + '">
+                                                </TableBody>
+                                            </Table>
+                                        </TableContainer>
+                                        <br />
+                                        <Typography variant="h6" gutterBottom component="div" marginLeft={100}>
+                                            總金額 : {total} 台幣
+                                        </Typography>
+                                    </Paper>
+                                </Box>
+                                {/* <table className="table table-hover table-responsive" id="' + idCartTable + '">
                                 {products && products.map(item => (
                                     <tr title="' + this.summary + '" data-id="' + this.id + '" data-price="' + this.price + '">
                                         <td>{item.name}</td>
@@ -440,17 +471,22 @@ const Cart = ({ setItemCount }) => {
                                     <td></td>
                                 </tr>
                             </table> */}
-                        </div>
-                        <div className="modal-footer">
-                            {/* <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">X</button> */}
-                            <button type="button" className="btn btn-pink" data-bs-dismiss="modal" onClick={handleCheckout}>結帳</button>
+                            </div>
+                            <div className="modal-footer">
+                                {/* <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">X</button> */}
+                                <button type="button" className="btn btn-pink" data-bs-dismiss="modal" onClick={handleCheckout}>結帳</button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </div >
             </div >
-        </div >
         </ThemeProvider>
     )
 }
+const Cart = styled(Element)`
 
+.css-1q1u3t4-MuiTableRow-root.Mui-selected{
+    color: pink
+}
+`
 export default Cart;
