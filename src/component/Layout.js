eaf7e49e0d335a350d7beaba6968883c0c17a9bc
dommei <yuchiehkao928@gmail.com>
2022-02-08 16:15:20 +0800

    update

--- src/component/Layout.js
+++ src/component/Layout.js
@@ -3,7 +3,11 @@ import Header from './Header'
 import Footer from './Footer'
 
 const Layout = ({ className, children }) => (
-    <div>
+    <div style={{
+        backgroundImage: `url("https://png.pngtree.com/back_origin_pic/05/14/94/2e1139be16239b483874dec1a4bebb6b.jpg")`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '100% 100%'
+    }}>
         <Header />
         <div className={className}>{children}</div>
         <Footer />
